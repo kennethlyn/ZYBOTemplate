7318f27ec957fc4327974795189f38572bfa8178
boxyoman <jon.covert@me.com>
2015-01-22 23:17:54 -0500

    No longer build a vivado project. Yay!

--- makefile
+++ makefile
@@ -1,14 +1,7 @@
-project: proj/
+project: ./build/output.bit
 
-open: proj/
-	@echo "Opening proj/proj.xpr"
-	@vivado proj/proj.xpr -nojournal -nolog > /dev/null
-
-proj/: ./hdl
-	@echo "Creating project"
-	@vivado -mode batch -source tcl/project.tcl -nojournal -nolog > /dev/null
-	@echo "Done creating project"
-	@echo "Don't know when anything else will be done"
+./build/output.bit: ./hdl
+	vivado -mode batch -source tcl/project.tcl -nojournal -nolog 
 
 clean:
-	rm -r proj
+	rm -r build
--- tcl/project.tcl
+++ tcl/project.tcl
@@ -1,99 +1,24 @@
-# This will generate the ./proj directory
-# Other scripts will run synthesis and implementation
-#
-# Set the reference directory for source file relative paths (by default the value is script directory path)
-set origin_dir "."
-source "$origin_dir/settings.tcl"
+source ./settings.tcl
 
-# Set the directory path for the original project from where this script was exported
-set orig_proj_dir "[file normalize "$origin_dir/proj"]"
+set build ./build
+file mkdir $build
 
-# Create project
-create_project proj ./proj -force
+#read_vhdl [ glob ./hdl/vhdl/*.vhd ]
+read_vhdl [ glob ./hdl/vhdl/*.vhdl ] 
 
-# Set the directory path for the new project
-set proj_dir [get_property directory [current_project]]
+#read_verilog [ glob ./hdl/verilog/*.v ]
+read_xdc [ glob ./hdl/contraints/ZYBO_MASTER.xdc ]
 
-# Set project properties
-set obj [get_projects proj]
-set_property "default_lib" "xil_defaultlib" $obj
-set_property "part" $part $obj
-set_property "simulator_language" "Mixed" $obj
-set_property "target_language" "VHDL" $obj
+synth_design -top $topLevel -part $part
+write_checkpoint -force $build/post_synth.dcp
 
-# Create 'sources_1' fileset (if not found)
-if {[string equal [get_filesets -quiet sources_1] ""]} {
-  create_fileset -srcset sources_1
-}
+opt_design
+place_design
 
-# Set 'sources_1' fileset object
-# Set 'sources_1' fileset properties
-set obj [get_filesets sources_1]
-set files [ list \
-	 "[file normalize "$origin_dir/hdl/vhdl/"]"\
-	 "[file normalize "$origin_dir/hdl/verilog/"]"\
-	 "[file normalize "$origin_dir/hdl/bd/"]"\
-]
-add_files -fileset $obj $files
+write_checkpoint -force $build/post_place.dcp
+report_utilization -file $build/post_place_util.dcp
 
-# Set the top level
-set obj [get_filesets sources_1]
-set_property "top" $topLevel $obj
-set_property "TOP_FILE" $topFile $obj
+route_design
+write_checkpoint -force $build/post_route.dcp
 
-# Create 'constrs_1' fileset (if not found)
-if {[string equal [get_filesets -quiet constrs_1] ""]} {
-  create_fileset -constrset constrs_1
-}
-
-# Set 'constrs_1' fileset object
-set obj [get_filesets constrs_1]
-
-# Empty (no sources present)
-read_xdc $origin_dir/hdl/contraints/ZYBO_MASTER.xdc
-
-# Set 'constrs_1' fileset properties
-set obj [get_filesets constrs_1]
-
-# Create 'sim_1' fileset (if not found)
-if {[string equal [get_filesets -quiet sim_1] ""]} {
-  create_fileset -simset sim_1
-}
-
-# Set 'sim_1' fileset object
-set obj [get_filesets sim_1]
-# Empty (no sources present)
-
-# Set 'sim_1' fileset properties
-set obj [get_filesets sim_1]
-
-# Create 'synth_1' run (if not found)
-if {[string equal [get_runs -quiet synth_1] ""]} {
-  create_run -name synth_1 -part $part -flow {Vivado Synthesis 2014} -strategy "Vivado Synthesis Defaults" -constrset constrs_1
-} else {
-  set_property strategy "Vivado Synthesis Defaults" [get_runs synth_1]
-  set_property flow "Vivado Synthesis 2014" [get_runs synth_1]
-}
-set obj [get_runs synth_1]
-set_property "part" $part $obj
-
-# set the current synth run
-current_run -synthesis [get_runs synth_1]
-
-# Create 'impl_1' run (if not found)
-if {[string equal [get_runs -quiet impl_1] ""]} {
-  create_run -name impl_1 -part $part -flow {Vivado Implementation 2014} -strategy "Vivado Implementation Defaults" -constrset constrs_1 -parent_run synth_1
-} else {
-  set_property strategy "Vivado Implementation Defaults" [get_runs impl_1]
-  set_property flow "Vivado Implementation 2014" [get_runs impl_1]
-}
-set obj [get_runs impl_1]
-set_property "part" $part $obj
-
-# set the current impl run
-current_run -implementation [get_runs impl_1]
-
-puts "INFO: Project created:proj"
-
-launch_runs synth_1
-launch_runs impl_1  -to_step write_bitstream 
+write_bitstream -force $build/output.bit
